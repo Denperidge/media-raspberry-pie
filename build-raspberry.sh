07369aa4e67985b99752cf5bcee811d61e906a44
Denperidge <denperidge@gmail.com>
2020-06-03 19:48:42 +0200

    fixed missing $ in build-rpi

--- build-raspberry.sh
+++ build-raspberry.sh
@@ -1,7 +1,7 @@
 # We will store all the files in the path configured in .env
 # Everything from config to the video files 
 media_path=$(cat .env | grep MEDIA_PATH= | cut -d '=' -f2)
-mkdir -p media_path
+mkdir -p $media_path
 cd "$media_path"
 
 mkdir "downloads"  # Downloads: stored here after download in qbittorrent
