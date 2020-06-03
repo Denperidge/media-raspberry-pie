f4b9ba2d373e59889e8ac770bf5bcf8d20f06703
Denperidge <denperidge@gmail.com>
2020-06-03 21:20:13 +0200

    Second draft of category sorting

--- post-download.sh
+++ post-download.sh
@@ -1,9 +1,9 @@
 clamscan -r "$1" > "/logs/$3-$2-($(date +"%d-%m-%Y--%H-%M-%S")).log"
-$category=$(echo "$3" | tr "[:upper:]" "[:lower:]")
-if [[ $3 ==  *"radarr"*]]; 
+$category="$(echo "$3" | tr "[:upper:]" "[:lower:]")"
+if [[ $category ==  *"radarr"*]]
 then
   mv "$1" /scanned/radarr/
-elif [[ $3 ==  *"sonarr"*]];
+elif [[ $category ==  *"sonarr"*]]
 then
   mv "$1" /scanned/sonarr/
 fi
