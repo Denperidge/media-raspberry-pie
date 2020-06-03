3f400f1d078b4f4e68742e06c87fee3b93a3f3a0
Denperidge <denperidge@gmail.com>
2020-06-03 17:48:02 +0200

    Improved mkdirs, added catgry dirs, simplified smb

--- build-raspberry.sh
+++ build-raspberry.sh
@@ -1,15 +1,20 @@
 # We will store all the files in the path configured in .env
 # Everything from config to the video files 
-arrpath=$(cat .env | grep PIEPATH= | cut -d '=' -f2)
-cd "$arrpath"
+media_path=$(cat .env | grep PIEPATH= | cut -d '=' -f2)
+mkdir -p media_path
+cd "$media_path"
 
 mkdir "downloads"  # Downloads: stored here after download in qbittorrent
 mkdir "torrents"  #  Torrents: .torrent files are kept here
-mkdir -m 777 "scanned"  #  Scanned: stored here after CLAMAV has scanned. Kept here until transcoder moves them
-mkdir -m 777 "transcoded"  # After transcoder is finished, files are moved here and kept there until moved by 
-mkdir "tv"  # Directory to keep tv show files after being downloaded, scanned and transcoded
-mkdir "movies"  # Directory to keep movies files after being downloaded, scanned and transcoded
-mkdir "clamav-logs"  # Clamav will store logs here 
+mkdir -p -m 1777 "scanned"  #  Scanned: stored here after CLAMAV has scanned. Kept here until transcoder moves them
+mkdir -p -m 1777 "scanned/tv-sonarr"
+mkdir -p -m 1777 "scanned/radarr"
+mkdir -p -m 1777 "transcoded"  # After transcoder is finished, files are moved here and kept there until moved by 
+mkdir -p -m 1777 "transcoded/tv-sonarr"
+mkdir -p -m 1777 "transcoded/radarr"
+mkdir -p "tv"  # Directory to keep tv show files after being downloaded, scanned and transcoded
+mkdir -p "movies"  # Directory to keep movies files after being downloaded, scanned and transcoded
+mkdir -p "clamav-logs"  # Clamav will store logs here 
 
 curl "https://raw.githubusercontent.com/Denperidge/media-raspberry-pie/master/post-download.sh" > downloads/post-download.sh
 chmod +x downloads/post-download.sh
@@ -20,7 +25,7 @@ sudo docker-compose up --detach
 clear
 echo "Open qbittorrent (ip:8080) > tools > options > downloads > Run external program on torrent completion"
 echo "Enable it and insert the following line:"
-echo "/bin/bash /downloads/post-download.sh \"%R\" \"%N\""
+echo "/bin/bash /downloads/post-download.sh \"%R\" \"%N\" \"%C\""
 echo "and press Save"
 read
 
@@ -31,24 +36,20 @@ sudo apt-get install samba samba-common-bin
 echo "downloads setup complete, now setup for transcoding!"
 echo "A samba share will now be set up, used by the transcoder to transcode"
 echo "/etc/samba/smb.conf will now be opened. Copy the the following lines and add them to the bottom."
-echo [to-transcode]
+echo [scanned]
 echo Comment = Folder to allow transcoder to reach files
-echo Path = $arrpath/scanned
+echo Path = $media_path/scanned
 echo Browseable = yes
 echo Writeable = Yes
 echo only guest = no
-echo create mask = 0660
-echo directory mask = 0660
 echo Public = yes
 echo
 echo [transcoded]
 echo Comment = Folder to allow transcoder to reach files
-echo Path = $arrpath/transcoded
+echo Path = $media_path/transcoded
 echo Browseable = yes
 echo Writeable = Yes
 echo only guest = no
-echo create mask = 0660
-echo directory mask = 0660
 echo Public = yes
 read
 sudo nano /etc/samba/smb.conf
