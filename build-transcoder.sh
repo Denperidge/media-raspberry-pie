0365bc2ffb9dc594df560f6cad2ed88e0615f175
Denperidge <denperidge@gmail.com>
2020-06-03 19:52:58 +0200

    build-transcoder now pulls files from repo

--- build-transcoder.sh
+++ build-transcoder.sh
@@ -1,7 +1,11 @@
 transcoder_path=$(cat .env | grep TRANSCODER_PATH= | cut -d '=' -f2)
 mkdir -p $transcoder_path
-cp {transcode.sh,process-sonarr.py,process-radarr.py,.env} $transcoder_path
+cp .env $transcoder_path
 cd $transcoder_path
+
+curl "https://raw.githubusercontent.com/Denperidge/media-raspberry-pie/master/transcode.sh" > transcode.sh
+curl "https://raw.githubusercontent.com/Denperidge/media-raspberry-pie/master/transcode.sh" > process-sonarr.py
+curl "https://raw.githubusercontent.com/Denperidge/media-raspberry-pie/master/transcode.sh" > process-radarr.py
 
 # Clone mp4 automator and install requirements
 git clone https://github.com/mdhiggins/sickbeard_mp4_automator.git repo
