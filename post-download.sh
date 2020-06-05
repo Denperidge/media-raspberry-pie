e6b2b20212f46762c4992c67e6ffea0b95808d5c
Denperidge <denperidge@gmail.com>
2020-06-05 16:07:59 +0200

    Fixed syntax errors in post-download

--- post-download.sh
+++ post-download.sh
@@ -1,9 +1,7 @@
 clamscan -r "$1" > "/logs/$3-$2-($(date +"%d-%m-%Y--%H-%M-%S")).log"
 category="$(echo "$3" | tr "[:upper:]" "[:lower:]")"
-if [[ $category ==  *"radarr"*]]
-then
+if [[ $category ==  *"radarr"* ]]; then
   mv "$1" /scanned/radarr/
-elif [[ $category ==  *"sonarr"*]]
-then
+elif [[ $category ==  *"sonarr"* ]]; then
   mv "$1" /scanned/sonarr/
 fi
