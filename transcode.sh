96b0dd0a709e093ad26ab8ffb168364e1f7516f6
Denperidge <denperidge@gmail.com>
2020-06-05 18:37:38 +0200

    More specific and easier paths for /import

--- transcode.sh
+++ transcode.sh
@@ -5,7 +5,7 @@ media_path=$(cat .env | grep MEDIA_PATH= | cut -d '=' -f2)
 cd $transcoder_path
 source $transcoder_path/m4avenv/Scripts/activate
 py -3 $transcoder_path/repo/manual.py -i //$pie_ip/to-transcode/sonarr/ -m //$pie_ip/transcoded/sonarr/ --auto
-py -3 $transcoder_path/process.py "$transcoder_path/repo/" "/import/sonarr/" sonarr
+py -3 $transcoder_path/process.py "$transcoder_path/repo/" "/import/" sonarr
 
 py -3 $transcoder_path/repo/manual.py -i //$pie_ip/to-transcode/radarr/ -m //$pie_ip/transcoded/radarr/ --auto
-py -3 $transcoder_path/process.py "$transcoder_path/repo/" "/import/radarr/" radarr
+py -3 $transcoder_path/process.py "$transcoder_path/repo/" "/import/" radarr
