1f37ad7971598f75fa56504604185af4e9100b77
Denperidge <denperidge@gmail.com>
2020-06-05 18:13:16 +0200

    Added media_path and fixed wrong path in transcode

--- transcode.sh
+++ transcode.sh
@@ -1,10 +1,11 @@
 transcoder_path=$(cat .env | grep TRANSCODER_PATH= | cut -d '=' -f2)
 pie_ip=$(cat .env | grep PIE_IP= | cut -d '=' -f2)
+media_path=$(cat .env | grep MEDIA_PATH= | cut -d '=' -f2)
 
 cd $transcoder_path
 source $transcoder_path/m4avenv/Scripts/activate
 py -3 $transcoder_path/repo/manual.py -i //$pie_ip/to-transcode/sonarr/ -m //$pie_ip/transcoded/sonarr/ --auto
-py -3 $transcoder_path/process.py "$transcoder_path/repo/" "$MEDIA_PATH/to-transcode/sonarr/" sonarr
+py -3 $transcoder_path/process.py "$transcoder_path/repo/" "$media_path/transcoded/sonarr/" sonarr
 
 py -3 $transcoder_path/repo/manual.py -i //$pie_ip/to-transcode/radarr/ -m //$pie_ip/transcoded/radarr/ --auto
-py -3 $transcoder_path/process.py "$transcoder_path/repo/" "$MEDIA_PATH/to-transcode/radarr/" radarr
+py -3 $transcoder_path/process.py "$transcoder_path/repo/" "$media_path/transcoded/radarr/" radarr
