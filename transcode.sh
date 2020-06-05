13cbe4a036122afd822ab9c3b19f6510552abffb
Denperidge <denperidge@gmail.com>
2020-06-05 16:28:55 +0200

    Fixed variable names in transcode.sh

--- transcode.sh
+++ transcode.sh
@@ -1,10 +1,10 @@
-transcoderpath=$(cat .env | grep TRANSCODERPATH= | cut -d '=' -f2)
-pieip=$(cat .env | grep PIEIP= | cut -d '=' -f2)
+transcoder_path=$(cat .env | grep TRANSCODER_PATH= | cut -d '=' -f2)
+pie_ip=$(cat .env | grep PIE_IP= | cut -d '=' -f2)
 
-cd $transcoderpath
+cd $transcoder_path
 source m4avenv/Scripts/activate
-py -3 repo/manual.py -i //$pieip/to-transcode/sonarr/ -m //$pieip/transcoded/sonarr/ --auto
+py -3 repo/manual.py -i //$pie_ip/to-transcode/sonarr/ -m //$pie_ip/transcoded/sonarr/ --auto
 py -3 process-sonarr.py
 
-py -3 repo/manual.py -i //$pieip/to-transcode/radarr/ -m //$pieip/transcoded/radarr/ --auto
+py -3 repo/manual.py -i //$pie_ip/to-transcode/radarr/ -m //$pie_ip/transcoded/radarr/ --auto
 py -3 process-radarr.py
