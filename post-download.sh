4cbe805c1fc3936bcaf24024ed8127a4b1620f28
Denperidge <denperidge@gmail.com>
2020-06-06 17:28:04 +0200

    Fixed incorrect mkdir command

--- post-download.sh
+++ post-download.sh
@@ -1,8 +1,9 @@
 clamscan -r "$1" > "/logs/$3-$2-($(date +"%d-%m-%Y--%H-%M-%S")).log"
 category="$(echo "$3" | tr "[:upper:]" "[:lower:]")"
-mkdir -p "/scanned/radarr/$1/"
 if [[ $category ==  *"radarr"* ]]; then
+  mkdir -p "/scanned/radarr/$1/"
   mv "$1" "/scanned/radarr/$1/$1"
 elif [[ $category ==  *"sonarr"* ]]; then
+  mkdir -p "/scanned/sonarr/$1/"
   mv "$1" "/scanned/sonarr/$1/$1"
 fi
