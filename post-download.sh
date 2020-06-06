59b98004aef9181045e277c86e3b0f7c48d1de75
Denperidge <denperidge@gmail.com>
2020-06-06 17:29:51 +0200

    Fixed incorrect variable usage in post-download.sh

--- post-download.sh
+++ post-download.sh
@@ -1,9 +1,9 @@
 clamscan -r "$1" > "/logs/$3-$2-($(date +"%d-%m-%Y--%H-%M-%S")).log"
 category="$(echo "$3" | tr "[:upper:]" "[:lower:]")"
 if [[ $category ==  *"radarr"* ]]; then
-  mkdir -p "/scanned/radarr/$1/"
-  mv "$1" "/scanned/radarr/$1/$1"
+  mkdir -p "/scanned/radarr/$2/"
+  mv "$1" "/scanned/radarr/$2/$2"
 elif [[ $category ==  *"sonarr"* ]]; then
-  mkdir -p "/scanned/sonarr/$1/"
-  mv "$1" "/scanned/sonarr/$1/$1"
+  mkdir -p "/scanned/sonarr/$2/"
+  mv "$1" "/scanned/sonarr/$2/$2"
 fi
