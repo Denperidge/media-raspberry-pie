d82fba8775c76ca4f9ae4e4bc288276439794263
Denperidge <denperidge@gmail.com>
2020-06-06 17:11:31 +0200

    Added missing mkdir to post-download

--- post-download.sh
+++ post-download.sh
@@ -1,5 +1,6 @@
 clamscan -r "$1" > "/logs/$3-$2-($(date +"%d-%m-%Y--%H-%M-%S")).log"
 category="$(echo "$3" | tr "[:upper:]" "[:lower:]")"
+mkdir -p "/scanned/radarr/$1/
 if [[ $category ==  *"radarr"* ]]; then
   mv "$1" "/scanned/radarr/$1/$1"
 elif [[ $category ==  *"sonarr"* ]]; then
