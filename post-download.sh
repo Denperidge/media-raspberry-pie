28ca2347b046b8bf153a9f55a5c50bc58c8f9972
Denperidge <denperidge@gmail.com>
2020-06-06 16:50:00 +0200

    Added quotes for safety post-download.sh

--- post-download.sh
+++ post-download.sh
@@ -1,7 +1,7 @@
 clamscan -r "$1" > "/logs/$3-$2-($(date +"%d-%m-%Y--%H-%M-%S")).log"
 category="$(echo "$3" | tr "[:upper:]" "[:lower:]")"
 if [[ $category ==  *"radarr"* ]]; then
-  mv "$1" /scanned/radarr/$1/$1
+  mv "$1" "/scanned/radarr/$1/$1"
 elif [[ $category ==  *"sonarr"* ]]; then
-  mv "$1" /scanned/sonarr/$1/$1
+  mv "$1" "/scanned/sonarr/$1/$1"
 fi
