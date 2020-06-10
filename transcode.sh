190c2e469fbb96cc7ce54084c351942d000f0b14
Denperidge <denperidge@gmail.com>
2020-06-10 21:03:04 +0200

    Combined older method of transcode with newer vars

--- transcode.sh
+++ transcode.sh
@@ -1,22 +1,45 @@
-cd %transcoder_path%
-source %transcoder_path%/m4avenv/bin/activate
+# Assume windows if not Linux.
+# The script would need presumably modifications to run on Mac, yet I do not have a machine to test this on
+if [  $(uname -s) = "Darwin" ]; then
+    echo "This script isn't meant to natively run on Mac, since I cannot test it on there"
+    echo "Please modify the script to make sure that mac compatibility works"
+    exit 1
+elif [ $(uname -s) != "Linux" ]; then  
+    os="windows"
+    python="py -3"
+else
+    os="linux"
+    python="python3"
+fi
 
-smb="%transcoder_path%/mnt/"
+# Get environment variables & go to transcoder
+transcoder_path=$(cat .env | grep TRANSCODER_PATH= | cut -d '=' -f2)
+pie_ip=$(cat .env | grep PIE_IP= | cut -d '=' -f2)
+cd "$transcoder_path"
 
-cd "$smb/to-transcode/sonarr/"
+# Activate venv
+if [ $os = "windows" ]; then  
+    source "$transcoder_path/m4avenv/Scripts/activate"
+else 
+    source "$transcoder_path/m4avenv/bin/activate"
+fi
+
+# Process tv shows
+cd "//$pie_ip/to-transcode/sonarr/"
 for d in * ; do
-    logfile_transcode = "$smb/logs/[$(date +'%d-%m-%Y--%H-%M-%S')]-transcode-sonarr-$d.log"
-    logfile_process = "$smb/logs/[$(date +'%d-%m-%Y--%H-%M-%S')]-process-sonarr-$d.log"
+    logfile_transcode="//$pie_ip/logs/[$(date +'%d-%m-%Y--%H-%M-%S')]-transcode-sonarr-$d.log"
+    logfile_process="//$pie_ip/logs/[$(date +'%d-%m-%Y--%H-%M-%S')]-process-sonarr-$d.log"
 
-    python3 "%transcoder_path%/repo/manual.py" -i "$d" -m "$smb/transcoded/sonarr/$d/" --auto --preserverelative > logfile_transcode
-    python3 "%transcoder_path%/process.py" "%transcoder_path%/repo/" "$d/" sonarr > logfile_process
+    $python "$transcoder_path/repo/manual.py" -i "$d" -m "//$pie_ip/transcoded/sonarr/$d/" --auto --preserverelative > logfile_transcode
+    $python "$transcoder_path/process.py" "$transcoder_path/repo/" "$d/" sonarr > logfile_process
 done
 
-cd "$smb/to-transcode/radarr/"
+# Process movies
+cd "//$pie_ip/to-transcode/radarr/"
 for d in * ; do
-    logfile_transcode = "$smb/logs/[$(date +'%d-%m-%Y--%H-%M-%S')]-transcode-radarr-$d.log"
-    logfile_process = "$smb/logs/[$(date +'%d-%m-%Y--%H-%M-%S')]-process-radarr-$d.log"
+    logfile_transcode="//$pie_ip/logs/[$(date +'%d-%m-%Y--%H-%M-%S')]-transcode-radarr-$d.log"
+    logfile_process="//$pie_ip/logs/[$(date +'%d-%m-%Y--%H-%M-%S')]-process-radarr-$d.log"
 
-    python3 "%transcoder_path%/repo/manual.py" -i "$d" -m "$smb/transcoded/radarr/$d/" --auto --preserverelative > logfile_transcode
-    python3 "%transcoder_path%/process.py" "%transcoder_path%/repo/" "$d/" radarr > logfile_process
+    $python "$transcoder_path/repo/manual.py" -i "$d" -m "//$pie_ip/transcoded/radarr/$d/" --auto --preserverelative > logfile_transcode
+    $python "$transcoder_path/process.py" "$transcoder_path/repo/" "$d/" radarr > logfile_process
 done
