1e4f723d42e5ebebd7fb4f21e774f443d536da15
Denperidge <denperidge@gmail.com>
2020-06-10 11:54:01 +0200

    Now using linux python notation

--- transcode.sh
+++ transcode.sh
@@ -9,12 +9,12 @@ source %transcoder_path%/m4avenv/bin/activate
 
 cd "$smb/to-transcode/sonarr/"
 for d in * ; do
-    py -3 "%transcoder_path%/repo/manual.py" -i "$d" -m "$smb/transcoded/sonarr/$d/" --auto --preserverelative > "$smb/logs/transcode-process-sonarr-$d-$(date +'%d-%m-%Y--%H-%M-%S').log"
-    py -3 "%transcoder_path%/process.py" "%transcoder_path%/repo/" "$d/" sonarr >> "$smb/logs/transcode-process-sonarr-$d-$(date +'%d-%m-%Y--%H-%M-%S').log"
+    python3 "%transcoder_path%/repo/manual.py" -i "$d" -m "$smb/transcoded/sonarr/$d/" --auto --preserverelative > "$smb/logs/transcode-process-sonarr-$d-$(date +'%d-%m-%Y--%H-%M-%S').log"
+    python3 "%transcoder_path%/process.py" "%transcoder_path%/repo/" "$d/" sonarr >> "$smb/logs/transcode-process-sonarr-$d-$(date +'%d-%m-%Y--%H-%M-%S').log"
 done
 
 cd "$smb/to-transcode/radarr/"
 for d in * ; do
-    py -3 "%transcoder_path%/repo/manual.py" -i "$d" -m "$smb/transcoded/radarr/$d/" --auto --preserverelative > "$smb/logs/transcode-process-radarr-$d-$(date +'%d-%m-%Y--%H-%M-%S').log"
-    py -3 "%transcoder_path%/process.py" "%transcoder_path%/repo/" "$d/" radarr >> "$smb/logs/transcode-process-radarr-$d-$(date +'%d-%m-%Y--%H-%M-%S').log"
+    python3 "%transcoder_path%/repo/manual.py" -i "$d" -m "$smb/transcoded/radarr/$d/" --auto --preserverelative > "$smb/logs/transcode-process-radarr-$d-$(date +'%d-%m-%Y--%H-%M-%S').log"
+    python3 "%transcoder_path%/process.py" "%transcoder_path%/repo/" "$d/" radarr >> "$smb/logs/transcode-process-radarr-$d-$(date +'%d-%m-%Y--%H-%M-%S').log"
 done
