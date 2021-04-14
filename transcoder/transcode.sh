693f4ac9153aa3dc7e1b769e2d54317f825f572e
Stijn <denperidge@gmail.com>
2021-04-14 13:30:07 +0200

    Fixed linux support for transcode.sh
    Previously it only worked on Windows, due to //$pie_ip/ being formatted in a windows-style navigation

--- transcoder/transcode.sh
+++ transcoder/transcode.sh
@@ -17,24 +17,26 @@ transcoder_path=$(cat .env | grep TRANSCODER_PATH= | cut -d '=' -f2)
 pie_ip=$(cat .env | grep PIE_IP= | cut -d '=' -f2)
 cd "$transcoder_path"
 
-# Activate venv
+# Activate venv and set pie path for windows
 if [ $os = "windows" ]; then  
     source "$transcoder_path/m4avenv/Scripts/activate"
+    pie_path="//$pie_ip/"
 else 
     source "$transcoder_path/m4avenv/bin/activate"
+    pie_path=$(cat .env | grep PIE_PATH= | cut -d '=' -f2)
 fi
 
 logfile="//$pie_ip/logs/$(date +'%d-%m-%Y').log"
 
 # Process tv shows
-cd "//$pie_ip/to-transcode/sonarr/"
+cd "$pie_path/to-transcode/sonarr/"
 for d in * ; do
     $python "$transcoder_path/repo/manual.py" -i "$d" -m "//$pie_ip/transcoded/sonarr/$d/" --auto --preserverelative >> "$logfile"
     $python "$transcoder_path/process.py" "$transcoder_path/repo/" "$d/" sonarr >> "$logfile"
 done
 
 # Process movies
-cd "//$pie_ip/to-transcode/radarr/"
+cd "$pie_path/to-transcode/radarr/"
 for d in * ; do
     $python "$transcoder_path/repo/manual.py" -i "$d" -m "//$pie_ip/transcoded/radarr/$d/" --auto --preserverelative >> "$logfile"
     $python "$transcoder_path/process.py" "$transcoder_path/repo/" "$d/" radarr >> "$logfile"
